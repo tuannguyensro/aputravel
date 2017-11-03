cb789ce9d4cf20649a848ab7e7e86eb6203d85ac
tuannguyensro <tuannguyensro@gmail.com>
2017-11-03 15:29:53 +0700

    15:29 3/11/2017

---
+++ admin/Controllers/BookingController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace admin.Controllers
+{
+    public class BookingController : Controller
+    {
+        //
+        // GET: /Booking/
+        public ActionResult Booking_List()
+        {
+            return View();
+        }
+	}
+}
